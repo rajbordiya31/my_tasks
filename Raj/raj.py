0edcf49b88c7d66034767901b04e744df7fa88a9
rajbordiya31 <raj.bordiya@gammaedge.io>
2025-07-23 16:46:14 +0530

    files added in new branch

---
+++ Raj/raj.py
@@ -0,0 +1,13 @@
+def geeks():
+    print("GeeksForGeeks")
+
+location= "noida"
+
+class Employees():
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def show(self):
+        print("Employee Name :", self.name)
+        print("Employee's Age :", self.age)
+        
